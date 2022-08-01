74d29f5b63406f7bc241aa9978adeb2dab47c89d
mae masters <maemastersdev@gmail.com>
2022-08-01 20:31:23 +0100

    extracted createEpisodeCode & listAnEpisode to Top

--- src/components/ListEpisodes.tsx
+++ src/components/ListEpisodes.tsx
@@ -4,43 +4,44 @@ import { textInputFilter } from "./searchEpisodes";
 import { useState } from "react";
 
 export function ListAllEpisodes(): JSX.Element {
+  
+  const [text, setText] = useState("");
+  const searchedEpisodeData: IEpisode[] = episodes.filter((oneEpisode) =>
+    textInputFilter(oneEpisode, text)
+  );
 
+  const ListedAllEpisodes = searchedEpisodeData.map(ListAnEpisode);
 
-    function createEpisodeCode(oneEpisode: IEpisode) {
-        const seasonCode = oneEpisode.season.toString().padStart(2, '0');
-        const episodeCode = oneEpisode.number.toString().padStart(2, '0');
-        return `S${seasonCode}E${episodeCode}`
-    }
+  return (
+    <>
+      <input
+        className="searchBar"
+        value={text}
+        onChange={(event) => {
+          setText(event.target.value);
+        }}
+      />
 
-    function listAnEpisode(oneEpisode: IEpisode ) {
-        return (
-            <div className="episodeCard">
-            <h1>{oneEpisode.name}</h1>
-            <h2>{createEpisodeCode(oneEpisode)}</h2>
-            <img src={oneEpisode.image.medium}/>
-            <p>{oneEpisode.summary}</p>
-            </div>
-        );
-    }
-
-    const [text, setText] = useState("");
-    const searchedEpisodeData: IEpisode[] = episodes.filter((oneEpisode) =>
-    textInputFilter(oneEpisode, text));
-
-    const ListedAllEpisodes = searchedEpisodeData.map(listAnEpisode);
-
-    return (
-        <>
-        <input
-          className= "searchBar"
-          value={text}
-          onChange={(event) => {
-            setText(event.target.value);
-          }}/>
-        
-        {ListedAllEpisodes}
-        </>
-    );
+      {ListedAllEpisodes}
+    </>
+  );
 }
 
-export default ListAllEpisodes
+function ListAnEpisode(oneEpisode: IEpisode) {
+  return (
+    <div className="episodeCard">
+      <h1>{oneEpisode.name}</h1>
+      <h2>{createEpisodeCode(oneEpisode)}</h2>
+      {oneEpisode.image !== null&& <img src={oneEpisode.image.medium} />}
+      <p>{oneEpisode.summary}</p>
+    </div>
+  );
+}
+
+function createEpisodeCode(oneEpisode: IEpisode) {
+  const seasonCode = oneEpisode.season.toString().padStart(2, "0");
+  const episodeCode = oneEpisode.number.toString().padStart(2, "0");
+  return `S${seasonCode}E${episodeCode}`;
+}
+
+export default ListAllEpisodes;
