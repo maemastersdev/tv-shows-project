62e541cb6999769b66781231d1b6d77e99cf3db3
mae masters <maemastersdev@gmail.com>
2022-08-01 14:33:28 +0100

    made season codes, written component display each episode, CSS to do

--- src/components/ListEpisodes.tsx
+++ src/components/ListEpisodes.tsx
@@ -3,11 +3,18 @@ import episodes from "../tvShowsData.json";
 
 export function ListAllEpisodes(): JSX.Element {
 
+    function createEpisodeCode(oneEpisode: IEpisode) {
+        const seasonCode = oneEpisode.season.toString().padStart(2, '0');
+        const episodeCode = oneEpisode.number.toString().padStart(2, '0');
+        return `S${seasonCode}E${episodeCode}`
+    }
+
     function listAnEpisode(oneEpisode: IEpisode ) {
         return (
             <div className="episodeCard">
             <h1>{oneEpisode.name}</h1>
-            <img src={oneEpisode.image.original}/>
+            <h2>{createEpisodeCode(oneEpisode)}</h2>
+            <img src={oneEpisode.image.medium}/>
             <p>{oneEpisode.summary}</p>
             </div>
         );
