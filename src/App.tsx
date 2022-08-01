10a8fadffb111cb77e8c5b7d172aec9a0c0afed6
mae masters <maemastersdev@gmail.com>
2022-08-01 11:43:39 +0100

    data json import check

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import { greet } from "./utils/greet";
+import episodes from "./tvShowsData.json"
+
+console.log(`Imported ${episodes.length} episode(s)`);
+console.log(`First episode's name is ${episodes[0].name}`);
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  return <h1> TV </h1>
 }
 
 export default App;
