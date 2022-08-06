7c8095b6b1125fb6e5e93c3e8744a98e1f0c3587
mae masters <maemastersdev@gmail.com>
2022-08-06 19:55:37 +0100

    Additional function to remove Ps and Ds from Shows

--- src/utils/removeThePs.ts
+++ src/utils/removeThePs.ts
@@ -1,6 +1,6 @@
 import { IEpisode } from "./Interfaces";
 
-export default function removeThePs(oneEpisode: IEpisode): string | null {
+export default function removeThePsAndDs(oneEpisode: IEpisode): string | null {
   if (oneEpisode.summary != null) {
     oneEpisode.summary = oneEpisode.summary.replace("<p>", "");
     oneEpisode.summary = oneEpisode.summary.replace("</p>", "");
